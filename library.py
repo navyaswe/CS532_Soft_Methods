cc076be1284a7c95afa1bc711a93db24f6a1c76e
Navya <122176358+navyaswe@users.noreply.github.com>
2023-10-25 15:17:46 -0700

    Update library.py

--- library.py
+++ library.py
@@ -11,6 +11,8 @@ from sklearn.impute import KNNImputer
 sklearn.set_config(transform_output="pandas")  #says pass pandas tables through pipeline instead of numpy matrices
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+
 
 #Custom Mapping Transformer
 class CustomMappingTransformer(BaseEstimator, TransformerMixin):
